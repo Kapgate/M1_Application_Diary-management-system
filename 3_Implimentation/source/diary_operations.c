72e11c5344885a99f30a1b2578067baaa3bb1384
Kapgate <94162941+Kapgate@users.noreply.github.com>
2021-11-22 13:40:13 +0530

    Create diary_operations.c

---
+++ 3_Implimentation/source/diary_operations.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+#include<conio.h>
+
+#include<string.h>
+
+int password();
+
+void addrecord();
+
+void viewrecord();
+
+void editrecord();
+
+void editpassword();
+
+void deleterecord();
+
+struct record
+
+{
+
+    char time[6];
+
+    char name[30];
+
+    char place[25];
+
+    char duration[10];
+
+    char note[500];
+
+} ;
